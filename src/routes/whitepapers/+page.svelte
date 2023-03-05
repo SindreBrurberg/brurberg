183fbc20d540537eaa50d7f6994b4b36c272204d
sindrebrurberg <sindre@brurberg.no>
2023-03-05 16:45:40 +0900

    Starting transform to brurberg branding

---
+++ src/routes/whitepapers/+page.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>Whitepapers</title>
+	<meta name="description" content="Whitepapers prodused by Brurberg" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Whitepapers</h1>
+
+	<p>
+		This page will contain cards for every whitepaper we write. 
+	</p>
+</div>
